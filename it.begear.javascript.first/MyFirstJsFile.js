c05b1493b226ad6ae41661c61d3a3f1a50510dd7
Saverio P. Zangaro <spzangaro@gmail.com>
2018-05-28 21:03:15 +0200

    Concluso training FreecodeCamp su JavaScript, iniziato training su jQuery.

--- it.begear.javascript.first/MyFirstJsFile.js
+++ it.begear.javascript.first/MyFirstJsFile.js
@@ -33,3 +33,58 @@ function reusableFunction() {
 }
 reusableFunction();
 
+//Confronto restrittivo, fa distinzione tra carattere e numero cosa che non farebbe == .
+//Setup
+function testStrict(val) {
+  if (val === 3) { // Change this line
+    document.write("Equal");
+  }
+  else{
+	document.write("Not Equal");	  
+  }
+}
+// Change this value to test
+testStrict('3');
+
+//Oggetto in JavaScript
+var testObj = {
+		"nome" : "Rino",
+		"numero" : 34,
+}
+//accesso agli oggetti
+document.write("<br>" + testObj.nome);
+//Bracket Notation
+document.write("<br>" + testObj["nome"]);
+//aggiungo proprietà ad un oggetto
+testObj["occhi"] = "verdi";
+document.write("<br>" + testObj.occhi);
+//Elimino proprietà
+delete testObj.occhi;
+document.write("<br>" + testObj.occhi);
+
+//JSON. Java Script Object Notation
+var myMusic = [
+	  {
+	    "artist": "Billy Joel",
+	    "title": "Piano Man",
+	    "release_year": 1973,
+	    "formats": [ 
+	      "CS", 
+	      "8T", 
+	      "LP" ],
+	    "gold": true
+	  },
+	  // Add record here
+	  {
+	  "artist": "Daft Punk",
+	  "title": "Homework",
+	  "release_year": 1997,
+	  "formats": [ 
+	    "CD",
+	    "Cassette",
+	    "LP"
+	  ],
+	  "gold": true
+	}
+];
+document.write("<br>" + myMusic[0].artist);
